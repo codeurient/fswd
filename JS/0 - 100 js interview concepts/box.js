112a0741552dc5c103877f45a1066f661c75b2c7
codeurient <codeurient@gmail.com>
2026-01-07 15:10:32 +0400

    projects for students

---
+++ JS/0 - 100 js interview concepts/box.js
@@ -0,0 +1,3 @@
+export default function box(size) {
+    return `Box ölçüsü: ${size}`;
+}
---
+++ JS/0 - 100 js interview concepts/main.js
@@ -0,0 +1,11 @@
+import { sum, pi } from './math.js';
+
+console.log(sum(2, 3));  // 5
+console.log(pi);         // 3.14
+
+
+
+
+import box from './box.js';
+
+console.log(  box(10)  ); 
